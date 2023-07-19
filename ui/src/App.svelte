2c49f01f75c4e23ff65a308ea4261378753c596b
Prasen Patil <prasen.patil.official@gmail.com>
2023-07-19 20:55:34 +0530

    first work

---
+++ ui/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Table from "./Table.svelte";
+	 let firstName='test';
+	 let lastName='p';
+	 let person = prompt("Please enter your name", "Harry Potter");
+	 $: name = person
+</script>
+
+<main>
+	<h1>Hello {name}</h1>
+
+	<Table/>
+	
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ ui/src/Table.svelte
@@ -0,0 +1,39 @@
+<script>
+    let columns = ['Heading 1','Heading 2','Heading 4','Heading 5','add'];
+    let values=[
+        {1:'test1',2:'test2',4:'test4',5:'test5',6:"<button>ttest</button>"},
+        {1:'test1',2:'test2',4:'test4',5:'test5',6:"<button>ttest</button>"},
+    ]
+  </script >
+
+<body>
+    <table>
+      <thead>
+        <tr>
+            {#each columns as columnHeading}
+              <th>{columnHeading}</th>
+            {/each}
+          <tr/>
+        {#each Object.values(values) as row}
+            <tr>
+            {#each Object.values(row) as cell}
+                <td>{cell}</td>
+            {/each}
+            </tr>
+        {/each}
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </body>   
+  <style>
+    table, th, td {
+      border: 1px solid;
+      border-collapse: collapse;
+      margin: 10px;
+    }
+  </style>
+  
